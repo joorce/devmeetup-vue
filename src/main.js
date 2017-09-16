f4d8d137bc4f7d289aaf27cf7d253c0cecab54ab
Jose Ortega <joseortega@joseortega.es>
2017-09-16 06:35:49 +0200

    Navigation toolbar setup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from './App'
-import router from './router'
+import Vue from "vue"
+import Vuetify from "vuetify"
+import App from "./App"
+import router from "./router"
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "@/components/Home"
+import Meetups from "@/components/meetup/Meetups"
+import CreateMeetup from "@/components/meetup/CreateMeetup"
+import Profile from "@/components/User/Profile"
+import Signup from "@/components/User/Signup"
+import Signin from "@/components/User/Signin"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/meetups",
+      component: Meetups
+    },
+    {
+      path: "/meetup/new",
+      component: CreateMeetup
+    },
+    {
+      path: "/profile",
+      component: Profile
+    },
+    {
+      path: "/signup",
+      component: Signup
+    },
+    {
+      path: "/signin",
+      component: Signin
     }
-  ]
+  ],
+  mode: "history"
 })
